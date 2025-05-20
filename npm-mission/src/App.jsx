57d1734399941de92b3995fbf9cea0745029b8ec
YJ-Y <yooyungjoo1@gmail.com>
2025-05-20 18:39:44 +0900

    2주차 미션 1

--- npm-mission/src/App.jsx
+++ npm-mission/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import Button from './Button';
+
 /*
 function App() {
   const [count, setCount] = useState(0)
@@ -33,14 +34,51 @@ function App() {
     </>
   )
 } */
-function App() {
+/* function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>블로그 버튼</h1>
       <Button />
     </div>
   );
+} */
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [increment, setIncrement] = useState(1);
+
+  const handleIncrease = () => {
+    setCount(count + increment);
+  };
+
+  const handleSetIncrement = (value) => {
+    setIncrement(value);
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1>GDG 카운터</h1>
+      <h2>{count}</h2>
+      <div style={styles.row}>
+        {[1, 10, 100].map((num) => (
+          <Button key={num} value={num} onClick={handleSetIncrement} />
+        ))}
+      </div>
+      <Button value="증가" onClick={handleIncrease} />
+    </div>
+  );
 }
 
+const styles = {
+  container: {
+    textAlign: "center",
+    marginTop: "40px"
+  },
+  row: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "18px",
+    marginBottom: "16px"
+  }
+};
 
-export default App
+
